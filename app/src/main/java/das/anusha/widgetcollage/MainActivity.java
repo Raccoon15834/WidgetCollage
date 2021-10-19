d3dbfbf85edf00ddb10b09341df3c7f42f38cb5e
Raccoon15834 <anusha.das4fx@gmail.com>
2021-10-19 14:15:27 -0400

    set fragment

--- app/src/main/java/das/anusha/widgetcollage/MainActivity.java
+++ app/src/main/java/das/anusha/widgetcollage/MainActivity.java
@@ -1,19 +1,35 @@
 package das.anusha.widgetcollage;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentTransaction;
+import androidx.viewpager2.adapter.FragmentStateAdapter;
 
 import android.os.Bundle;
+import android.widget.ScrollView;
 
 public class MainActivity extends AppCompatActivity {
-
+    String[] SKILLSETS;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        SKILLSETS = getResources().
+                getStringArray(R.array.skillsets);
+        String tagPrefix = "ss";
+        for(String i: SKILLSETS){
+            FragmentTransaction myFt = getSupportFragmentManager().beginTransaction()
+                    .add(R.id.sSetScroll, SkillSetFrag.newInstance(i), tagPrefix+i);
+        }
     }
+
 }
 
 //Submit each layout screenshot
 //P3L07aDasAnusha.apk
 //embelishments apk and video
 //TODO transition later
+//TODO colors, logo, skillsets descriptins?
+//TODO transitions when frag leaves returns, plus button animation
--- app/src/main/java/das/anusha/widgetcollage/SkillSetFrag.java
+++ app/src/main/java/das/anusha/widgetcollage/SkillSetFrag.java
@@ -1,7 +1,41 @@
 package das.anusha.widgetcollage;
 
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ScrollView;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
 
+import org.w3c.dom.Text;
+
 public class SkillSetFrag extends Fragment {
+    private View.OnClickListener mySSlistener;
+    //TODO add listener that can communicate with activity
+    //guides.codepath.com/android/creating-and-using-fragments
+    String title;
+
+    public static SkillSetFrag newInstance(String title){
+        SkillSetFrag myNewFrag= new SkillSetFrag();
+        myNewFrag.title = title;
+        return myNewFrag;
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        return inflater.inflate(R.layout.skillsetfrag, container, false);
+    }
+
+    @Override
+    public void onViewCreated(@NonNull View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+        TextView myTitle = view.findViewById(R.id.titleTxt);
+        myTitle.setText(title);
+    }
 
 }
